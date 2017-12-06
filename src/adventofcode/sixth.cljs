a12398eec46156adab39f8c4637cfe0236e096ff
antoinezimmermann <antoine.zimmermann@webedia-group.com>
2017-12-06 18:09:13 +0100

    WIP : need to refactor shift vect method

--- src/adventofcode/sixth.cljs
+++ src/adventofcode/sixth.cljs
@@ -15,12 +15,20 @@
 (def ones (repeat 1))
 (def zeros (repeat 0))
 
+(defn shift [offset vect]
+  (let [length (count vect)
+        head (drop (- length offset) vect)
+        tail (take offset vect)]
+    (apply vector (concat head tail))))
+
 (defn partition-memory [memory size index]
   (if (< memory size)
-    (concat 
-      (take (- (+ memory index) size) ones)
-      (take index zeros)
-      (take (+ index (- size memory)) ones))
+    (let [vect (concat
+                 '(0)
+                 (take memory ones)
+                 (take (- size memory) zeros))
+          shifted-vect (shift index vect)]
+      shifted-vect)
 
     (let [div (dec size)
           remaining (mod memory div)
@@ -31,10 +39,27 @@
         remaining))))
 
 
+(defn update-memory [current-state update-vect index]
+  (apply vector (map + (assoc current-state index 0) update-vect)))
+
 (defn solution []
-  (let [target (get-max input)
-        index (.indexOf input target)]
-    (println 
-      target
-      index)
-    (partition-memory target size index)))
+  (loop [history #{}
+         state input]
+
+    (if (contains? history state)
+      (count history)
+
+      (let [target (get-max state)
+            index (.indexOf state target)
+            update-vect (partition-memory target size index)
+            new-state (update-memory state update-vect index)]
+
+        (println 
+          target
+          index
+          update-vect
+          new-state)
+
+        (recur
+          (conj history state)
+          new-state)))))
