ce33fed44f7dc8d73bc09cf5818e1aff3125f81c
antoinezimmermann <antoine.zimmermann@webedia-group.com>
2017-12-11 13:58:25 +0100

    wip : solution below
    Each move on the grid moves either :
    - 1 step further origin
    - 1 setp closer origin (come back but by the same path)
    - move but stay at the same distance
    
    For each move you can model it by a vector of integer, after each move
    you need to rotate your vector in order to take the new position into
    account

--- src/adventofcode/core.cljs
+++ src/adventofcode/core.cljs
@@ -3,4 +3,41 @@
 
 (nodejs/enable-util-print!)
 
-(println "hello world")
+(def move-north [1
+                 1
+                 0
+                 -1
+                 0
+                 0])
+
+(def x [1 0 0 0 0 0])
+
+(defn rotate [vect n]
+  (let [head (take n vect)
+        tail (drop n vect)]
+    (apply vector (concat tail head))))
+
+(def zeros (repeat 0))
+
+(defn vect-k [dim k]
+  (apply vector (concat (take k zeros)
+                          '(1)
+                          (take (- dim k) zeros))))
+
+; (def rotation-matrix [(rotate 1 x)
+;                       x
+;                       (rotate 2 x)])
+
+(def identity-matrix (apply vector (map (partial vect-k 5) (range 6))))
+
+(defn mat-product [mat vect]
+  (apply vector (map
+                  (fn [line]
+                    (reduce
+                      (partial +)
+                      (map vector line vect)))
+                  mat)))
+
+
+(println identity-matrix)
+(println (rotate identity-matrix 1))
