5647889092256ce65c0af2d3216a5c15dd21c689
antoinezimmermann <antoine.zimmermann@webedia-group.com>
2017-12-23 14:24:40 +0100

    first part

--- src/adventofcode/core.cljs
+++ src/adventofcode/core.cljs
@@ -2,6 +2,11 @@
   (:require 
             [cljs.nodejs :as nodejs]
             [lumo.io :refer [slurp]]
+            [clojure.pprint :refer [pprint]]
+            [cljs.test :refer-macros [deftest
+                                      is
+                                      testing
+                                      run-tests]]
             [clojure.string :refer [split]]))
 
 (def input (split 
@@ -21,15 +26,17 @@
             "<=" <=})
 
 (defn process-tokens [registers
-                      [target cmd valueS _ reg pred operande]]
+                      [target cmd value _ reg pred operande]]
 
-  ; (println target cmd valueS reg pred operande)
-  (let [value (int valueS)
-        curr-reg-val (get registers reg)
+
+  (let [offset (int value)
         predicate (get preds pred)
-        pred-val (predicate curr-reg-val (int operande))]
+        update-reg (get cmds cmd)
+        pred-val (predicate 
+                   (int (get registers reg))
+                   (int operande))]
     (if pred-val
-      (update registers target (get cmds cmd) value)
+      (update registers target #(update-reg % offset))
       registers)))
 
 (nodejs/enable-util-print!)
@@ -37,9 +44,9 @@
 (def final-state (reduce process-tokens {}
                          (map #(split % " ") input)))
 
-(def reversed-map (apply hash-map 
-                         (apply concat (map #(list (get final-state %) %)
-                                                     (keys final-state)))))
-
-(def m (apply min (keys reversed-map)))
-(println (get reversed-map m))
+; (def reversed-map (apply hash-map 
+;                          (apply concat (map #(list (get final-state %) %)
+;                                                      (keys final-state)))))
+(pprint final-state)
+(def sol1 (apply max (map second final-state)))
+(println sol1)
