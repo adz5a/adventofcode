c82ac2fd56380e2e41ac77e5463e1a2aede5cc91
antoinezimmermann <antoine.zimmermann@webedia-group.com>
2017-12-25 00:23:15 +0100

    second part

--- src/adventofcode/core.cljs
+++ src/adventofcode/core.cljs
@@ -8,6 +8,7 @@
                                       testing
                                       run-tests]]
             [clojure.string :refer [split]]))
+(nodejs/enable-util-print!)
 
 (def input (split 
              (slurp "src/adventofcode/input.txt")
@@ -28,7 +29,6 @@
 (defn process-tokens [registers
                       [target cmd value _ reg pred operande]]
 
-
   (let [offset (int value)
         predicate (get preds pred)
         update-reg (get cmds cmd)
@@ -39,14 +39,28 @@
       (update registers target #(update-reg % offset))
       registers)))
 
-(nodejs/enable-util-print!)
 
-(def final-state (reduce process-tokens {}
-                         (map #(split % " ") input)))
+; (def final-state (reduce process-tokens {}
+;                          (map #(split % " ") input)))
 
 ; (def reversed-map (apply hash-map 
 ;                          (apply concat (map #(list (get final-state %) %)
 ;                                                      (keys final-state)))))
-(pprint final-state)
-(def sol1 (apply max (map second final-state)))
-(println sol1)
+; (pprint final-state)
+; (def sol1 (apply max (map second final-state)))
+; (println sol1)
+
+(defn process2 [[maximums registers]
+                tokens]
+  ; (println registers)
+  (let [next-registers (process-tokens registers tokens)]
+    ; (println next-registers)
+    [(conj maximums (apply max (map second next-registers)))
+     next-registers]))
+
+
+(def final-state2 (reduce process2
+                          [#{} {}]
+                          (map #(split % " ") input)))
+
+(println (apply max (first final-state2)))
