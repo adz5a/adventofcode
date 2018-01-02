3a6fe2fd04fe07eddcbce78f9278475f76b17b29
antoinezimmermann <antoine.zimmermann@webedia-group.com>
2018-01-02 13:57:09 +0100

    wip

--- src/adventofcode/core.cljs
+++ src/adventofcode/core.cljs
@@ -6,8 +6,47 @@
                                       run-tests]]))
 
 (nodejs/enable-util-print!)
+(defn flip [pattern]
+  ;; pattern [l1 l2]
+ (let [lines (apply map vector pattern)]
+   (apply vector (map #(->> %
+                           (reverse)
+                           (apply vector)) lines))))
 
-(deftest test:example
-  (is (= 1 1)))
+(nodejs/enable-util-print!)
+
+(deftest test:flip
+  (is (= [
+          [1 1 1]
+          [2 2 2]
+          [3 3 3]
+          ]
+         (flip [
+                [1 2 3]
+                [1 2 3]
+                [1 2 3]
+                ])))
+
+
+  (is (=
+       [
+          [3 2 1]
+          [3 2 1]
+          [3 2 1]
+        ]
+       (flip (flip [
+                    [1 2 3]
+                    [1 2 3]
+                    [1 2 3]
+                    ]))))
+
+  (is (=
+       [[4 3]
+        [3 4]]
+       (flip [
+              [3 1]
+              [4 2]
+              ])))
+  )
 
 (run-tests)
